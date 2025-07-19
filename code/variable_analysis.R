7c80aebf0c4cca5efc23d3f3b9d7d93cc823848a
Sona Tau Estrada Rivera <sona@stau.space>
2025-07-19 00:58:39 -0400

    feat: Variable analysis is DONE
    Signed-off-by: Sona Tau Estrada Rivera <sona@stau.space>

--- code/variable_analysis.R
+++ code/variable_analysis.R
@@ -13,26 +13,86 @@ library(SnowballC) # wordStem
 # install.packages("gglot2")
 library(ggplot2) # ggplot
 
-load("google_sema_AMS.Rda")
-load("google_sema_CBMS.Rda")
-load("google_sema_IPEDS.Rda")
-load("stem_AMS.Rda")
-load("stem_CBMS.Rda")
-load("stem_IPEDS.Rda")
 
+load("data/stem_AMS.Rda")
+stem_count_ams <- stem_count
+load("data/stem_CBMS.Rda")
+stem_count_cbms <- stem_count
+load("data/stem_IPEDS.Rda")
+stem_count_ipeds <- stem_count
 
-data <- cbind(ams,cbms,ipeds)
-
-pdf("plots2.pdf", onefile = TRUE)
-for (row_idx in 1:nrow(data)) {
-	print(ggplot(stem_count, aes(x = reorder(word, n), y = n)) +
+data_sources <- list(stem_count_ams,stem_count_cbms,stem_count_ipeds)
+names <- list("AMS", "CBMS", "IPEDS")
+i <- 1
+pdf("plots_stem.pdf", onefile = TRUE)
+for (data in data_sources) {
+	data <- data[complete.cases(data),]
+	print(ggplot(data, aes(x = reorder(word, n), y = n)) +
 		geom_col() +
 		coord_flip() +
-		labs(title = title))
-	print(ggplot(sema_count, aes(x = reorder(word_category, n), y = n)) +
+		labs(title = paste("Most frequent words in", names[[i]], "surveys")))
+	i <- i + 1
+}
+dev.off()
+
+load("data/glove_100d_AMS.Rda")
+sema_count_glove_100d_ams <- sema_count
+load("data/glove_100d_CBMS.Rda")
+sema_count_glove_100d_cbms <- sema_count
+load("data/glove_100d_IPEDS.Rda")
+sema_count_glove_100d_ipeds <- sema_count
+load("data/glove_200d_AMS.Rda")
+sema_count_glove_200d_ams <- sema_count
+load("data/glove_200d_CBMS.Rda")
+sema_count_glove_200d_cbms <- sema_count
+load("data/glove_200d_IPEDS.Rda")
+sema_count_glove_200d_ipeds <- sema_count
+load("data/glove_300d_AMS.Rda")
+sema_count_glove_300d_ams <- sema_count
+load("data/glove_300d_CBMS.Rda")
+sema_count_glove_300d_cbms <- sema_count
+load("data/glove_300d_IPEDS.Rda")
+sema_count_glove_300d_ipeds <- sema_count
+load("data/glove_50d_AMS.Rda")
+sema_count_glove_50d_ams <- sema_count
+load("data/glove_50d_CBMS.Rda")
+sema_count_glove_50d_cbms <- sema_count
+load("data/glove_50d_IPEDS.Rda")
+sema_count_glove_50d_ipeds <- sema_count
+load("data/google_sema_AMS.Rda")
+sema_count_google_ams <- sema_count
+load("data/google_sema_CBMS.Rda")
+sema_count_google_cbms <- sema_count
+load("data/google_sema_IPEDS.Rda")
+sema_count_google_ipeds <- sema_count
+
+data_sources <- list(sema_count_glove_100d_ams,sema_count_glove_100d_cbms,sema_count_glove_100d_ipeds,sema_count_glove_200d_ams,sema_count_glove_200d_cbms,sema_count_glove_200d_ipeds,sema_count_glove_300d_ams,sema_count_glove_300d_cbms,sema_count_glove_300d_ipeds,sema_count_glove_50d_ams,sema_count_glove_50d_cbms,sema_count_glove_50d_ipeds,sema_count_google_ams,sema_count_google_cbms,sema_count_google_ipeds)
+names <- list(
+	"AMS surveys w/ 100-D Glove model",
+	"CBMS surveys w/ 100-D Glove model",
+	"IPEDS surveys w/ 100-D Glove model",
+	"AMS surveys w/ 200-D Glove model",
+	"CBMS surveys w/ 200-D Glove model",
+	"IPEDS surveys w/ 200-D Glove model",
+	"AMS surveys w/ 300-D Glove model",
+	"CBMS surveys w/ 300-D Glove model",
+	"IPEDS surveys w/ 300-D Glove model",
+	"AMS surveys w/ 50-D Glove model",
+	"CBMS surveys w/ 50-D Glove model",
+	"IPEDS surveys w/ 50-D Glove model",
+	"AMS surveys w/ Google News model",
+	"CBMS surveys w/ Google News model",
+	"IPEDS surveys w/ Google News model"
+)
+
+i <- 1
+pdf("plots_sema.pdf", onefile = TRUE)
+for (data in data_sources) {
+	data <- data[complete.cases(data),]
+	print(ggplot(data, aes(x = reorder(tag, n), y = n)) +
 		geom_col() +
 		coord_flip() +
-		labs(title = paste("Most Frequent Categories in", data[row_idx,"title"], "surveys")))
-	print(title)
+		labs(title = paste("Most frequent tags in", names[[i]])))
+	i <- i + 1
 }
 dev.off()
