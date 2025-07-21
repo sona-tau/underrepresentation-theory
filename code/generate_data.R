aa9673d456e342265612e713bd0599048d57e0f9
Sona Tau Estrada Rivera <sona@stau.space>
2025-07-21 12:36:35 -0400

    fix: Data generating R file is now correct.
    Signed-off-by: Sona Tau Estrada Rivera <sona@stau.space>

--- code/generate_data.R
+++ code/generate_data.R
@@ -69,11 +69,15 @@ rep <- function(str, count) {
 	out
 }
 
+# These are the tags that are used to categorize the data.
 lgbt_tags <- c("lgbt","lgbtq","sex","identity","gender","orientation","nonbinary") |> as.character()
 race_ethnicity_tags <- c("race","ethnicity","african","american","black","hispanic","asian","indigenous","native","latino","latina","latine") |> as.character()
 women_tags <- c("woman","women","girl","feminine","femeninity","ms","mrs") |> as.character()
 men_tags <- c("man", "men", "boy", "male", "masculine", "masculinity", "mr") |> as.character()
 disabilities_tags <- c("disabilities","disabled","disability","handicap","handicapped","neurodivergent") |> as.character()
+
+# This variable holds all of the tags. Additionally, tag_categories holds all
+# the tags together with their categories.
 tags <- c(lgbt_tags, race_ethnicity_tags, women_tags, men_tags, disabilities_tags)
 tag_categories <- c(
 	rep("lgbt", length(lgbt_tags)),
@@ -82,9 +86,9 @@ tag_categories <- c(
 	rep("men", length(men_tags)),
 	rep("disabilities", length(disabilities_tags))
 )
-similarity_values <- numeric(length = length(tags))
 
 # 
+similarity_values <- numeric(length = length(tags))
 word_category <- function(word, tag_vectors, emb, threshold = 0.3) {
 	for (i in seq_along(tags)) {
 		similarity_values[i] <- word2vec_similarity(w2v(word, emb), tag_vectors[,i], type = "cosine")
